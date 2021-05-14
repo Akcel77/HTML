e0a6dc24fc8697deb7fa5ff519d802609b37ae1d
Akcel77 <diagneaxel@contact.com>
2021-05-14 18:26:52 +0200

    Commit HTML

---
+++ SiteCv/js/main.js
@@ -0,0 +1,13 @@
+$('.navTrigger').click(function () {
+    $(this).toggleClass('active');
+    console.log("Clicked menu");
+    $("#mainListDiv").toggleClass("show_list");
+    $("#mainListDiv").fadeIn();
+
+});
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });
